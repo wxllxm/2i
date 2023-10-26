fcf3b7dd540608e5214ea2f14428e02cce028adb
wxllxm <william.deeley@hotmail.com>
2023-10-26 21:01:33 +0100

    Added updates to fix problems after a coffee break and some chin scratching

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const numberPairs = (theNumber, theArray) => {
   let pairsCount = 0;
 
-  theArray.forEach((arrayNum, index) => {
-    for (let i = 1; i < theArray.length; i++) {
-      if (arrayNum + theArray[i] == theNumber) {
+  //Changed outer loop so I have control over the indexing after removing elements from the array.
+  //I noticed for some reason the index started at 1, this has been changed to 0.
+  for (let i = 0; i < theArray.length; i++) {
+    for (let j = i + 1; j < theArray.length; j++) {
+      if (theArray[i] + theArray[j] === theNumber) {
         console.log(
-          `${arrayNum} & ${theArray[i]} equal the magic number of ${theNumber}. Adding to pair count.`
+          `${theArray[i]} & ${theArray[j]} equal the magic number of ${theNumber}. Adding to pair count.`
         );
 
-        theArray.splice(index, 1);
-        theArray.splice(theArray[i], 1);
         pairsCount += 1;
+
+        //Using the same loop types make it easier to read. 'i & j' and not 'index & theArray[i]'.
+        theArray.splice(j, 1);
+        theArray.splice(i, 1);
+
         console.log(`Numbers left... ${theArray}`);
+
+        //I didnt break out of the first loop in v1. I think this is why it was not removing the correct elements from the array.
+        break;
       }
     }
-  });
+  }
 
   console.log(`Total pair count: ${pairsCount}`);
 };
@@ -23,3 +31,11 @@ const myNumber = 42;
 const myArray = [1, 1, 10, 32, 41];
 
 numberPairs(myNumber, myArray);
+
+/**
+ *
+ * This function loops through the array. Takes 'i' and sees if it equals 'the magic number' when getting added to 'j' from the inner loop.
+ * If this is true, then the pair count is +1'd and the numbers are removed from the array.
+ * Rinse and repeat until all pairs are found.
+ * Volia.
+ */
